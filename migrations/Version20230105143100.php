8d18ed50b1568013a1d25d64c64fb4ea03cce316
Hugo Boulhaut <boulhaut.hugo@gmail.com>
2023-01-07 17:26:14 +0100

    Heroku

--- migrations/Version20230105143100.php
+++ migrations/Version20230105143100.php
@@ -20,18 +20,12 @@ final class Version20230105143100 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C7294869C');
-        $this->addSql('DROP INDEX IDX_9474526C7294869C ON comment');
-        $this->addSql('ALTER TABLE comment DROP article_id');
         $this->addSql('ALTER TABLE membre ADD reduction INT DEFAULT NULL, ADD point_total INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment ADD article_id INT NOT NULL');
-        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C7294869C FOREIGN KEY (article_id) REFERENCES article (id)');
-        $this->addSql('CREATE INDEX IDX_9474526C7294869C ON comment (article_id)');
         $this->addSql('ALTER TABLE membre DROP reduction, DROP point_total');
     }
 }
