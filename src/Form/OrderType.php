4e1463086e026146427223bfeae6c5d9ab2597a0
Hugo Boulhaut <boulhaut.hugo@gmail.com>
2023-01-07 18:45:44 +0100

    Heroku

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -34,12 +34,6 @@ class OrderType extends AbstractType
                 'expanded' => true
 
             ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Valider ma commande', 
-                'attr' => [
-                    'class' => 'btn btn-primary w-100'
-                ]
-            ])
         ;
     }
 
