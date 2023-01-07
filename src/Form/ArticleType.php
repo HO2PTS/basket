d6a4cd324cab3f35a387e5ca3cf6ab06e6844621
Hugo Boulhaut <boulhaut.hugo@gmail.com>
2023-01-07 16:33:50 +0100

    debug KEditor

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,11 +4,11 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
-use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ArticleType extends AbstractType
 {
@@ -16,7 +16,11 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('titre')
-            ->add('description', CKEditorType::class)
+            ->add('description', TextType::class, [
+                'attr' => [
+                    'style' => 'height: 200px;'
+                ]
+            ])
             ->add('citation')
             ->add('photo', FileType::class, [
                 'label' => 'Image du produit',
